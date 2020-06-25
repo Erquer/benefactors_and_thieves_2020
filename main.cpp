d1cf79a5622105ba9ed5700224b7756f2f32e616
Ppiotr98 <pppiotr98@gmail.com>
2020-06-25 19:56:59 +0200

    Delete Benefactor and Thieve classes. Move their methods to main

--- main.cpp
+++ main.cpp
@@ -9,8 +9,13 @@
 
 #include "utils.h"
 #include "communication.h"
-#include "benefactor.h"
-#include "thieve.h"
+
+#define NOTREQUESTING 0
+#define REQUESTING 1
+#define FLOWERPOT 0
+#define TOILET 1
+#define BROKEN 0
+#define REPAIRED 1
 
 //Globals
 int lamport_clock = 0;
@@ -34,16 +39,18 @@ bool run_program = true;
 //debug mode - show additional messages while working
 bool debugMode = true;
 
-//benefactor - if process is benefactor then we initialize this variable to control requests
-Benefactor* benefactor;
-//thieve - if process is thieve then we initialise this variable to control requests.
-Thieve* thieve;
-
 //wysłanie requesta = request + indeks + changeStamp.
 
 /*
  * Vectors needed.
  */
+
+//0 -> process not requesting resources
+//1 -> process requesting a resource
+bool processStatus = NOTREQUESTING;
+
+//lamport clock
+int processLamport = 0;
 
 //Requests to flowerpots
 std::vector<Request> potsRequests;
@@ -124,6 +131,155 @@ void checkThread(int *argc, char **argv[]){
 
 }
 
+////////////////////////////////////////////
+//////////// PROCESS FUNCTIONS /////////////
+////////////////////////////////////////////
+
+//choose to fix flowerpot or toilet
+//0 -> flowerpot
+//1 -> toilet
+bool flowerpotOrToilet() {
+    //create broken flowerpots counter
+    int brokenFlowerpotsCount = 0;
+    //count
+    //for each flowerpot in potStatus
+    for (auto flowerpot : potStatus){
+        if (flowerpot->status == BROKEN){
+            brokenFlowerpotsCount++;
+        }
+    }
+
+    //create broken toilets counter
+    int brokenToiletsCount = 0;
+    //count
+    //for each toilet in toilStatus
+    for (auto toilet : toilStatus){
+        if (toilet->status == BROKEN){
+            brokenToiletsCount++;
+        }
+    }
+
+    //requests counts
+    int flowerpotRequestsCount = potsRequests.size();
+    int toiletRequestsCount = toilRequests.size();
+
+    //calculate requests to resources ratio
+    double flowerpotsRatio = (brokenFlowerpotsCount == 0) ? 0.0 : flowerpotRequestsCount / brokenFlowerpotsCount;
+    double toiletsRatio = (brokenToiletsCount == 0) ? 0.0 : toiletRequestsCount / brokenToiletsCount;
+
+    //choose smaller ratio
+    bool choice;
+    if(flowerpotsRatio < toiletsRatio){
+        choice = FLOWERPOT;
+    }
+    else{
+        choice = TOILET;
+    }
+
+    return choice;
+}
+
+//find an item to fix 
+//return item index from flowerpots/toilets vector
+std::pair<int,int> findItemToFix() {
+    //choose to fix flowerpot or toilet
+    //0 -> flowerpot
+    //1 -> toilet
+    bool choice = flowerpotOrToilet();
+
+    //flowerpot choosen
+    if (choice == FLOWERPOT){
+        //hold smallest flowerpot changeStamp
+        //set this to the first flowerpot changeStamp value
+        Flowerpot* firstFlowerpot = potStatus[0];
+        int smallestChangeStampValue = firstFlowerpot->changeStamp;
+        int smallestChangeStampID = 0;
+
+        //for each flowerpot in potStatus
+        for(int flowerpotID = 0; flowerpotID < potStatus.size(); flowerpotID++){
+            //current flowerpot
+            Flowerpot* flowerpot = potStatus[flowerpotID];
+
+            //check if this flowerpot has no requests
+            bool noRequests = true;
+            //for each request in potsRequests
+            for(Request request : potsRequests){
+                //if request to this flowerpot exists break
+                if(request.rid == flowerpotID){
+                    noRequests = false;
+                    break;
+                }
+            }
+
+            //if flowerpot has no requests choose it as an item to fix
+            if(noRequests){
+                return std::make_pair(choice, flowerpotID);
+            }
+            //else keep looking for the smallest flowerpot changeStamp
+            else if(flowerpot->changeStamp < smallestChangeStampValue) {
+                smallestChangeStampValue = flowerpot->changeStamp;
+                smallestChangeStampID = flowerpotID;
+            }
+        } //for
+
+        //if every flowerpot have some requests
+        //return the one with the smallest changeStamp
+        return std::make_pair(choice, smallestChangeStampID);
+    } //if
+
+    //toilet choosen
+    else if (choice == TOILET){
+        //hold smallest toilet changeStamp
+        //set this to the first toilet changeStamp value
+        Toilet* firstToilet = toilStatus[0];
+        int smallestChangeStampValue = firstToilet->changeStamp;
+        int smallestChangeStampID = 0;
+
+        //for each toilet in toilets
+        for(int toiletID = 0; toiletID < toilStatus.size(); toiletID++){
+            //current toilet
+            Toilet* toilet = toilStatus[toiletID];
+
+            //check if this toilet has no requests
+            bool noRequests = true;
+            //for each request in toiletRequests
+            for(Request request : toilRequests){
+                //if request to this toilet exists break
+                if(request.rid == toiletID){
+                    noRequests = false;
+                    break;
+                }
+            }
+
+            //if toilet has no requests choose it as an item to fix
+            if(noRequests){
+                return std::make_pair(choice, toiletID);
+            }
+            //else keep looking for the smallest toilet changeStamp
+            else if(toilet->changeStamp < smallestChangeStampValue) {
+                smallestChangeStampValue = toilet->changeStamp;
+                smallestChangeStampID = toiletID;
+            }
+        } //for
+
+        //if every toilet have some requests
+        //return the one with the smallest changeStamp
+        return std::make_pair(choice, smallestChangeStampID);
+    } //else if
+    return std::make_pair(0,0);
+} //int Benefactor::findItemToFix()
+
+void runYourStuff(){
+    
+    std::pair<int,int> choice = findItemToFix();
+
+    printf("[Benefactor %d] Chosen %d item with id %d \n", myPID, choice.first, choice.second);
+
+    sleep(4);
+
+
+
+}
 
 /*
  * message structure: Lamport + PID + index of requested item + changeStamp of requested item
@@ -133,7 +289,6 @@ void checkThread(int *argc, char **argv[]){
  * argv[2] => liczba toalet
  */
 int main(int argc, char  *argv[]) {
-
 
     if(argc < 3){
         perror("Za mało argumentów\n");
@@ -149,20 +304,10 @@ int main(int argc, char  *argv[]) {
     //initialize vectors with flowerpots and toilets
     initData(argc, argv);
 
-    if(myPID % 2 == 0){
-        //thieve
-        thieve = new Thieve(potStatus, toilStatus);
-    }else{
-        //benefactor
-        benefactor = new Benefactor(potStatus,toilStatus, myPID);
-    }
     //seed for random based on our PID
     srand(myPID);
 
-    while (run_program)
-    {
-       benefactor->runYourStuff();
-    }
+    runYourStuff();
     
     std::cout << "Hello, World!" << std::endl;
 
